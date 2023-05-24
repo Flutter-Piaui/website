e70015ebf778b5214f7457aecb957b5889bfc6ec
viktorsht <viktorshantus@gmail.com>
2023-05-24 20:00:28 -0300

    Adcionando cores do projeto

--- lib/design_system/colors/color.dart
+++ lib/design_system/colors/color.dart
@@ -3,9 +3,21 @@ import 'package:flutter/material.dart';
 abstract class ColorsApp {
   //Seguir exemplo do branco
   Color get branco;
+  Color get azul;
+  Color get preto;
+  Color get cinza;
+  Color get cinzaEscuro;
 }
 
 class ColorsAppDefault implements ColorsApp {
   @override
   Color get branco => const Color(0xFFFFFFFF);
+  @override
+  Color get azul => const Color(0xFF0582ca);
+  @override
+  Color get preto => const Color(0xFF1A1A1A); 
+  @override
+  Color get cinza => const Color(0xFFEAEAEA);
+  @override
+  Color get cinzaEscuro => const Color(0xFF656565);
 }
