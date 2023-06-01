cbfc03e63559a1a263eb7b5484a6ac0c71ec160f
Daniel <daniel@MacBook-Pro-de-Enio-2.local>
2023-06-01 19:08:38 -0300

    feat/menu_home

---
+++ lib/design_system/components/menu/menu.dart
@@ -0,0 +1,112 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+import 'package:flutter_piaui_website/design_system/texts/texts.dart';
+
+class Menu extends StatelessWidget {
+  void Function() buttonInitial;
+  void Function() buttonAbout;
+  void Function() buttonPhotos;
+  void Function() buttonEvents;
+  void Function() buttonContacts;
+  Menu({
+    Key? key,
+    required this.buttonInitial,
+    required this.buttonAbout,
+    required this.buttonPhotos,
+    required this.buttonEvents,
+    required this.buttonContacts,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      final bool isMobile = constraints.maxWidth < 900;
+
+      if (isMobile) {
+        return menuMobile(buttonInitial, buttonAbout, buttonPhotos,
+            buttonEvents, buttonContacts);
+      } else {
+        return menuWeb(buttonInitial, buttonAbout, buttonPhotos, buttonEvents,
+            buttonContacts);
+      }
+    });
+  }
+}
+
+Widget menuWeb(
+    void Function() buttonInitial,
+    void Function() buttonAbout,
+    void Function() buttonPhotos,
+    void Function() buttonEvents,
+    void Function() buttonContacts) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      RichText(
+          text: TextSpan(children: [
+        TextSpan(text: 'Flutter', style: TextAppDefault().menuFlutter),
+        TextSpan(text: ' Piauí', style: TextAppDefault().menuPiaui)
+      ])),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          TextButton(
+              onPressed: buttonInitial,
+              child: Text('Início', style: TextAppDefault().menuSubtitle)),
+          TextButton(
+              onPressed: buttonAbout,
+              child: Text('Sobre', style: TextAppDefault().menuSubtitle)),
+          TextButton(
+              onPressed: buttonPhotos,
+              child: Text('Fotos', style: TextAppDefault().menuSubtitle)),
+          TextButton(
+              onPressed: buttonEvents,
+              child: Text('Eventos', style: TextAppDefault().menuSubtitle)),
+          TextButton(
+              onPressed: buttonContacts,
+              child: Text('Contatos', style: TextAppDefault().menuSubtitle)),
+        ],
+      )
+    ],
+  );
+}
+
+Widget menuMobile(
+    void Function() buttonInitial,
+    void Function() buttonAbout,
+    void Function() buttonPhotos,
+    void Function() buttonEvents,
+    void Function() buttonContacts) {
+  return Column(
+    children: [
+      RichText(
+          text: TextSpan(children: [
+        TextSpan(text: 'Flutter', style: TextAppDefault().menuFlutter),
+        TextSpan(text: ' Piauí', style: TextAppDefault().menuPiaui)
+      ])),
+      Card(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton(
+                onPressed: buttonInitial,
+                child: Text('Início', style: TextAppDefault().menuSubtitle)),
+            TextButton(
+                onPressed: buttonAbout,
+                child: Text('Sobre', style: TextAppDefault().menuSubtitle)),
+            TextButton(
+                onPressed: buttonPhotos,
+                child: Text('Fotos', style: TextAppDefault().menuSubtitle)),
+            TextButton(
+                onPressed: buttonEvents,
+                child: Text('Eventos', style: TextAppDefault().menuSubtitle)),
+            TextButton(
+                onPressed: buttonContacts,
+                child: Text('Contatos', style: TextAppDefault().menuSubtitle)),
+          ],
+        ),
+      )
+    ],
+  );
+}
--- lib/design_system/texts/texts.dart
+++ lib/design_system/texts/texts.dart
@@ -5,15 +5,32 @@ import 'package:google_fonts/google_fonts.dart';
 abstract class TextApp {
   //Seguir exemplo texto basico
   TextStyle get basic;
+  TextStyle get menuFlutter;
+  TextStyle get menuPiaui;
+  TextStyle get menuSubtitle;
 }
 
 class TextAppDefault implements TextApp {
   //Obs: Alterar fonte, coloquei apenas para entenderem como declarar
   @override
   TextStyle get basic => GoogleFonts.poppins(
-    color: ThemeApp.colors.branco,
-    fontSize: 16,
-    fontWeight: FontWeight.bold,
-  );
+        color: ThemeApp.colors.branco,
+        fontSize: 16,
+        fontWeight: FontWeight.bold,
+      );
 
+  @override
+  TextStyle get menuFlutter => GoogleFonts.poppins(
+      fontSize: 40, fontWeight: FontWeight.bold, color: ThemeApp.colors.blue);
+
+  @override
+  TextStyle get menuPiaui => GoogleFonts.poppins(
+      color: ThemeApp.colors.black, fontSize: 40, fontWeight: FontWeight.bold);
+
+  @override
+  TextStyle get menuSubtitle => GoogleFonts.poppins(
+      color: ThemeApp.colors.black,
+      fontSize: 25,
+      fontWeight: FontWeight.w600,
+      );
 }
