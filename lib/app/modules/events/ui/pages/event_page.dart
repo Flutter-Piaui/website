703630ee2fc31b8af482d875bfa5756f62ca86e5
Danrley Silva <danrleysil46@gmail.com>
2023-06-18 17:34:47 -0300

    changes

--- lib/app/modules/events/ui/pages/event_page.dart
+++ lib/app/modules/events/ui/pages/event_page.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import '../../data/datasources/event_datasource.dart';
+
+import '../../domain/entities/event_entity.dart';
 import 'package:flutter_piaui_website/design_system/theme/website_images.dart';
+
 import '../../../../../design_system/components/carousel.dart';
 import '../../../../../design_system/components/custom_card.dart';
 import '../../../../../design_system/texts/texts.dart';
-import '../../data/stores/event/event_store.dart';
-
 import '../../../../../design_system/texts/texts.dart';
+import '../../data/stores/event/event_store.dart';
 
 class EventPage extends StatefulWidget {
   const EventPage({Key? key}) : super(key: key);
@@ -18,11 +21,16 @@ class EventPage extends StatefulWidget {
 
 class _EventPageState extends State<EventPage> {
   late final EventStore store;
-
+  List<EventEntity>? listEvent;
   @override
   void initState() {
     super.initState();
     store = Modular.get<EventStore>();
+    fetchListEvent();
+  }
+
+  fetchListEvent() async {
+    listEvent = await JsonEventDatasource().fetchEvents();
   }
 
   @override
@@ -59,55 +67,6 @@ class _EventPageState extends State<EventPage> {
               ],
             ),
           ),
-          // Expanded(
-          //     child: GridView.count(
-          //   crossAxisCount: 3,
-          //   children: [
-          //     ListView.builder(
-          //       scrollDirection: Axis.horizontal,
-          //       physics: NeverScrollableScrollPhysics(),
-          //       shrinkWrap: true, //
-          //       itemCount: WebsiteImages().imageList.length,
-          //       itemBuilder: (ctx, index) {
-          //         return Container(
-          //           margin: EdgeInsets.only(left: 20),
-          //           child: CustomCard(
-          //             cardType: CardType.event,
-          //             image: WebsiteImages().imageList[index],
-          //           ),
-          //         );
-          //       },
-          //     ),
-          //   ],
-          // )
-
-          // ...... other list children.
-          // ),
-          // Expanded(
-          //   child: ListView.builder(
-          //     // scrollDirection: Axis.horizontal,
-          //     itemCount: WebsiteImages().imageList.length,
-          //     itemBuilder: (ctx, index) {
-          //       return GridView.count(
-          //           crossAxisCount: 3,
-          //           physics:
-          //               NeverScrollableScrollPhysics(), // to disable GridView's scrolling
-          //           shrinkWrap: true, // You won't see infinite size error
-          //           children: <Widget>[
-          //             Container(
-          //               margin: EdgeInsets.only(left: 20),
-          //               child: Container(
-          //                 height: 24,
-          //                 child: CustomCard(
-          //                   cardType: CardType.event,
-          //                   image: WebsiteImages().imageList[index],
-          //                 ),
-          //               ),
-          //             )
-          //           ]);
-          //     },
-          //   ),
-          // )
         ]),
       ),
     );
